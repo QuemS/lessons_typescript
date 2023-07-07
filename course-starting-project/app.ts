8c1ec3f72763eb8ea263cda919a6618e97ec9811
Sushchenok Mykhailo <78119979+QuemS@users.noreply.github.com>
2023-07-07 13:15:42 +0300

    3 done

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,10 +1,16 @@
-function add(n1: number, n2: number) {
-    return n1 + n2
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+    const result = n1 + n2
+
+    if (showResult) {
+        console.log(phrase + result)
+    } else {
+        return result
+    }
 }
 
 const num1 = 5
 const num2 = 4
+const printResult = true
+const phraseText = 'Result is: '
 
-let result = add(num1, num2)
-
-console.log(result)
+add(num1, num2, printResult, phraseText)
